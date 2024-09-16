f619dd6462b39775b4b61cc03283d12c4a07c138
Seno Rama Dhani <racrozx11@gmail.com>
2024-09-16 22:05:57 +0700

    update

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 
-const colors = require('tailwindcss/colors');
+import colors from 'tailwindcss/colors';
 
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
